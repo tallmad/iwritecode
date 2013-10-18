1f5183c948f8bb777c39b091e99c3902359cacb6
gaofeng liang <gaofeng.liang@ele.me>
2013-10-18 16:44:30 +0800

    test not equal

--- tests/test_helloworld.py
+++ tests/test_helloworld.py
@@ -1,13 +1,13 @@
 # -*- coding:utf8 -*-
 import requests
 from nose import run
-from nose.tools import assert_equal
+from nose.tools import assert_not_equal
 
 
 def test_helloworld():
     r = requests.get('http://127.0.0.1:8888')
     print r.text
-    assert_equal(r.text, 'Hello, world')
+    assert_not_equal(r.text, 'Hello, world')
 
 
 if __name__ == '__main__':
